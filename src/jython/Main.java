9e1ad9926c90e61c6637b62b64710fcbb261806f
Eric Oestrich <eric@oestrich.org>
2011-04-26 16:52:49 -0400

    move methods out into a class

--- src/jython/Main.java
+++ src/jython/Main.java
@@ -4,17 +4,7 @@
  */
 package jython;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.LinkedList;
-import java.util.List;
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 /**
@@ -22,59 +12,12 @@ import javax.script.ScriptException;
  * @author eoestr0
  */
 public class Main {
-    private static List<IPrinter> printers = new LinkedList<IPrinter>();
-    
-    private static ScriptEngine pyEngine;
-    private static ScriptEngine rbEngine;
-    
-    private static Invocable pyIEngine;
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ScriptException, NoSuchMethodException, FileNotFoundException {
-        /* Get Scripting Engine */
-        ScriptEngineManager factory = new ScriptEngineManager();
-        pyEngine = factory.getEngineByName("python");
-        if (pyEngine == null) {
-            throw new RuntimeException("Could not find python Engine");
-        }
-        pyIEngine = (Invocable) pyEngine;
+        ScriptManager sm = new ScriptManager();
         
-        rbEngine = factory.getEngineByName("ruby");
-        if (rbEngine == null) {
-            throw new RuntimeException("Could not find ruby Engine");
-        }
-        loadScripts();
-        
-        for(int i = 0; i < 10; i++){
-            Update();
-        }
-    }
-
-    private static void loadScripts() throws ScriptException, FileNotFoundException, NoSuchMethodException {
-        File dir = new File("scripts");
-        
-        for(File file : dir.listFiles()){
-            InputStream is = new FileInputStream(file); // use for relative to application
-            Reader reader = new InputStreamReader(is);
-
-            int index = file.getName().lastIndexOf(".");
-            String extension = file.getName().substring(index);
-            
-            if(extension.equals(".py")){
-                pyEngine.eval(reader);
-                printers.add((IPrinter) pyIEngine.invokeFunction(file.getName().substring(0, index)));
-            }
-            else if(extension.equals(".rb")){
-                printers.add((IPrinter) rbEngine.eval(reader));
-            }
-        }
-    }
-    
-    public static void Update(){
-        for(IPrinter printer : printers){
-            printer.Update();
-        }
+        sm.run();
     }
 }
---
+++ src/jython/ScriptManager.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jython;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.LinkedList;
+import java.util.List;
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ *
+ * @author eoestr0
+ */
+public class ScriptManager {
+    private List<IPrinter> printers = new LinkedList<IPrinter>();
+    
+    private ScriptEngine pyEngine;
+    private ScriptEngine rbEngine;
+    
+    private Invocable pyIEngine;
+    
+    public void run() throws ScriptException, FileNotFoundException, NoSuchMethodException{
+        /* Get Scripting Engine */
+        ScriptEngineManager factory = new ScriptEngineManager();
+        pyEngine = factory.getEngineByName("python");
+        if (pyEngine == null) {
+            throw new RuntimeException("Could not find python Engine");
+        }
+        pyIEngine = (Invocable) pyEngine;
+        
+        rbEngine = factory.getEngineByName("ruby");
+        if (rbEngine == null) {
+            throw new RuntimeException("Could not find ruby Engine");
+        }
+        loadScripts();
+        
+        for(int i = 0; i < 10; i++){
+            Update();
+        }
+    }
+    
+    private void loadScripts() throws ScriptException, FileNotFoundException, NoSuchMethodException {
+        File dir = new File("scripts");
+        
+        for(File file : dir.listFiles()){
+            InputStream is = new FileInputStream(file); // use for relative to application
+            Reader reader = new InputStreamReader(is);
+
+            int index = file.getName().lastIndexOf(".");
+            String extension = file.getName().substring(index);
+            
+            if(extension.equals(".py")){
+                pyEngine.eval(reader);
+                printers.add((IPrinter) pyIEngine.invokeFunction(file.getName().substring(0, index)));
+            }
+            else if(extension.equals(".rb")){
+                printers.add((IPrinter) rbEngine.eval(reader));
+            }
+        }
+    }
+    
+    public void Update(){
+        for(IPrinter printer : printers){
+            printer.Update();
+        }
+    }
+}
